8b3e51f588893d019c736184c1cd3b534de999b8
r2ambg <45332789+r2ambg@users.noreply.github.com>
2022-07-13 02:27:36 +0200

    added guess taken limit

--- guess-the-number.py
+++ guess-the-number.py
@@ -1,14 +1,23 @@
 import random
 SecretNumb = random.randint(1, 100)
-print("the number that I have in my mind is between 1 to 100")
-for Gnumb in range(1, 101):
-    Gnumb = int(input("guess a number: "))
-    if Gnumb < SecretNumb :
-        print("your guess is too low")
-    elif Gnumb > SecretNumb :
-        print("your guess is too high")
-    elif Gnumb not in range(1, 101) :
-        print("not valid(out of range)")
-    else : 
-        print("you've won yohooo the number was %d" %(SecretNumb))
+Gnumb = int(input("guess a number between 1 to 100: "))
+
+while Gnumb in range(1, 101):
+    for guessesTaken in range(1, 5):
+        guessesTaken = guessesTaken+1
+        Gnumb = int(input("guess a number: "))
+        
+        if Gnumb < SecretNumb :
+            print("your guess is too low")
+            
+        elif Gnumb > SecretNumb :
+            print("your guess is too high")
+            
+        else : 
+            break
+        
+    if SecretNumb == Gnumb :
+        print("correct")     
+    else :
+        print("no more attempt")
         break
